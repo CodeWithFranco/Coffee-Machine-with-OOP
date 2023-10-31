db8d7fa4637dec839b2c3010912f4d2f5786df37
CodeWithFranco <120446758+CodeWithFranco@users.noreply.github.com>
2023-10-30 23:11:35 -0400

    Initial Files and code for the object-oriented programming version of Coffee machine

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
-#from menu import Menu, MenuItem
-#from coffee_maker import CoffeeMaker
-#from money_machine import MoneyMachine
+from menu import Menu, MenuItem
+from coffee_maker import CoffeeMaker
+from money_machine import MoneyMachine
 
-print("Hello Test")
+continue_brewing = True
+while continue_brewing:
+    user_selection = input('What would you like? (espresso/latte/cappuccino)\n').lower()
+    if user_selection == 'off':
+        continue_brewing = False
+    elif user_selection == 'report':
+        machine_report = CoffeeMaker()
+        # print object and methods
+        print(machine_report.report())
+
+
+
